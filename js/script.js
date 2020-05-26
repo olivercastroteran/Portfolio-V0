9b036e3901d6200a9e1d42f98d721c8f862b8fb3
olivercastroteran <olivercastroteran@gmail.com>
2020-05-26 19:57:39 -0400

    header finished

--- js/script.js
+++ js/script.js
@@ -6,4 +6,12 @@ $(document).ready(function () {
     play: 5000,
     pagination: false,
   });
+
+  var typed = new Typed('.typed', {
+    strings: ['Fron-End Developer', 'React Developer', 'UX/UI Designer'],
+    typeSpeed: 90,
+    loop: true,
+    startDelay: 1000,
+    showCursor: false,
+  });
 });
