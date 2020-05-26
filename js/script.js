4a2dabb7ce2785e725c5a670e9e55b3cc70c2e39
olivercastroteran <olivercastroteran@gmail.com>
2020-05-25 20:16:01 -0400

    image slider added

---
+++ js/script.js
@@ -0,0 +1,9 @@
+/* https://github.com/nicinabox/superslides */
+
+$(document).ready(function () {
+  $('#slides').superslides({
+    animation: 'fade',
+    play: 5000,
+    pagination: false,
+  });
+});
